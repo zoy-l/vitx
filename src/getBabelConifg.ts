4a06a48531584a433128da1c557abdf114de64a4
zoy <409626581@qq.com>
2021-03-15 02:19:49 +0800

    fix: pkg babel config

--- src/getBabelConifg.ts
+++ src/getBabelConifg.ts
@@ -14,6 +14,23 @@ export default function getBabelConfig(
     react
   } = bundleOpts
 
+  const defaultEnvConfig = {
+    exclude: [
+      'transform-member-expression-literals',
+      'transform-reserved-words',
+      'transform-template-literals',
+      'transform-typeof-symbol',
+      'transform-unicode-regex',
+      'transform-sticky-regex',
+      'transform-new-target',
+      'transform-modules-umd',
+      'transform-modules-systemjs',
+      'transform-modules-amd',
+      'transform-literals',
+      !isBrowser && 'transform-regenerator'
+    ].filter(Boolean)
+  }
+
   return {
     presets: [
       disableTypes && require.resolve('@babel/preset-typescript'),
@@ -23,7 +40,8 @@ export default function getBabelConfig(
           targets: isBrowser
             ? { browsers: ['last 2 versions', 'IE 10'] }
             : { node: nodeVersion ?? 6 },
-          modules: isBrowser ? false : 'auto'
+          modules: isBrowser ? false : 'auto',
+          ...defaultEnvConfig
         }
       ],
       isBrowser && react && '@babel/preset-react',
@@ -32,18 +50,18 @@ export default function getBabelConfig(
     plugins: [
       moduleType === 'cjs' &&
         !isBrowser && [
-          require.resolve('@babel/plugin-transform-modules-commonjs'),
+          require.resolve('@babel/plugin-transform-modules-commonjs'), // @babel/preset-env
           { lazy: true }
         ],
       require.resolve('@babel/plugin-proposal-export-default-from'),
       require.resolve('@babel/plugin-proposal-do-expressions'),
-      require.resolve('@babel/plugin-proposal-export-namespace-from'),
-      require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
-      require.resolve('@babel/plugin-proposal-optional-chaining'),
-      require.resolve('@babel/plugin-syntax-dynamic-import'),
-      [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+      require.resolve('@babel/plugin-proposal-export-namespace-from'), // @babel/preset-env
+      require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), // @babel/preset-env
+      require.resolve('@babel/plugin-proposal-optional-chaining'), // @babel/preset-env
+      require.resolve('@babel/plugin-syntax-dynamic-import'), // @babel/preset-env
+
       [
-        require.resolve('@babel/plugin-proposal-class-properties'),
+        require.resolve('@babel/plugin-proposal-class-properties'), // @babel/preset-env
         { loose: true }
       ],
       runtimeHelpers && [
