5cb176d220c36b5052621aa04ec3981722538527
zoy-l <409626581@qq.com>
2021-11-11 11:04:00 +0800

    fix: jest config

--- packages/vitx/src/jestTransformer.ts
+++ packages/vitx/src/jestTransformer.ts
@@ -2,7 +2,7 @@ import transformer from 'babel-jest'
 import getBabelConifg from './getBabelConifg'
 
 module.exports = transformer.createTransformer!({
-  ...getBabelConifg({ target: 'node', disableTypes: true }, false, 'cjs'),
+  ...getBabelConifg({ target: 'node' }, false, 'cjs'),
   babelrc: false,
   configFile: false
 })
