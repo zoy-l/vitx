52482ae8d3149356b0b427482bc055b43f9551ac
zoy-l <409626581@qq.com>
2021-12-31 18:59:47 +0800

    chore: use svg

--- packages/vitx-site/template/common/element/Simulator.jsx
+++ packages/vitx-site/template/common/element/Simulator.jsx
@@ -1,3 +1,11 @@
+function Sensor() {
+  return (
+    <svg viewBox="0 0 1024 1024" width="8" height="8">
+      <path d="M64 900v0.2C195.8 824 348.8 780.3 512 780.3s316.2 43.6 448 119.9v-0.2c0-331.6-180.2-621.2-448-776.1C244.2 278.8 64 568.3 64 900z" />
+    </svg>
+  )
+}
+
 export default function Simulator(props, { slots }) {
   const { children = slots.default?.() } = props
 
@@ -7,6 +15,8 @@ export default function Simulator(props, { slots }) {
         <div className="vitx-built-device__content">
           <div className="vitx-built-device__sensors">
             <div />
+            <Sensor />
+            <Sensor />
           </div>
           {children}
         </div>
